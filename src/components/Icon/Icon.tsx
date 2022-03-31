c1750a3ebb3d50bc6fae879f5be83ed8fefddbca
mmilad75 <55688834+mmilad75@users.noreply.github.com>
2022-04-01 00:24:00 +0430

    implement Icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import {useFonts} from 'expo-font';
+import {createIconSetFromFontello} from '@expo/vector-icons';
+import fontelloConfig from './config.json';
+
+const IconComponent = createIconSetFromFontello(fontelloConfig, 'ASIcon', 'asicon.ttf');
+
+interface Props {
+	name: string,
+	size?: number,
+	color?: string
+}
+
+const Icon: React.FC<Props> = ({name, size = 24, color = 'black'}) => {
+	const [fontsLoaded] = useFonts({
+		ASIcon: require('./asicon.ttf'),
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
+
+	return (
+		<IconComponent name={name} size={size} color={color} />
+	);
+};
+
+export default Icon;
