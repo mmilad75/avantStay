9ff5c0ce74b73a8ad06dd86159327b70f0e0a430
mmilad75 <55688834+mmilad75@users.noreply.github.com>
2022-04-04 02:51:07 +0430

    change activeOpacity of button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
 
 const Button: React.FC<TouchableOpacityProps> = ({children, ...props}) => (
-  <TouchableOpacity {...props}>
+  <TouchableOpacity activeOpacity={0.8} {...props}>
     {children}
   </TouchableOpacity>
 );
